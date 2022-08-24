8fb67bd074129238b7c1f96d1b3dfa97b488d374
halimwastaken <halim_gulesen@hotmail.com>
2022-08-24 09:13:00 +0300

    first commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Login_Page.Models;
+using System.Data.SqlClient;
+
+namespace Login_Page.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        void connectionString()
+        {
+            con.ConnectionString = "data source = DESKTOP-GROV6BS; database = LoginDataBase; integrated security = SSPI;";
+        }
+        [HttpPost]
+        public ActionResult Verify(Account acc)
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from [User] where UserName = '" + acc.Name + "' and Password = '" + acc.Password + "'   ";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return View("Create");
+            }
+            else
+            {
+                con.Close();
+                return View("Error");
+            }
+        }
+    }
+}
